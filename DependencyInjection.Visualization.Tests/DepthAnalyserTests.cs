52e15c3d97f46cc1598d36e0675f6d87df900937
jackkendall <jkendall3096@gmail.com>
2024-07-27 16:05:47 +0100

    Add basic depth analysis tests

---
+++ DependencyInjection.Visualization.Tests/DepthAnalyserTests.cs
@@ -0,0 +1,80 @@
+using DependencyInjection.Visualization;
+using Microsoft.Extensions.DependencyInjection;
+using FluentAssertions;
+
+public class DepthAnalyserTests
+{
+    private IServiceCollection CreateTestServices()
+    {
+        var services = new ServiceCollection();
+        services.AddTransient<IServiceA, ServiceA>();
+        services.AddScoped<IServiceB, ServiceB>();
+        services.AddSingleton<IServiceC, ServiceC>();
+        services.AddTransient<ServiceD>();
+        return services;
+    }
+    
+    public interface IServiceA { }
+    public class ServiceA : IServiceA { }
+    public interface IServiceB { }
+    public class ServiceB : IServiceB { public ServiceB(IServiceA service1) { } }
+    public interface IServiceC { }
+    public class ServiceC : IServiceC { public ServiceC(IServiceA service1, IServiceB service2) { } }
+    public class ServiceD { public ServiceD(IServiceC service3) { } }
+
+    [Fact]
+    public void GetRegistrationChainsByDepth_BasicFunctionality()
+    {
+        var services = CreateTestServices();
+        var tree = new DependencyTree(services, "DependencyInjection.Visualization");
+
+        var result = tree.GetRegistrationChainsByDepth(2);
+
+        result.RootNodes.Should().NotBeEmpty();
+        result.StringRepresentation.Should().NotBeNullOrWhiteSpace();
+        result.RootNodes.Should().Contain(n => n.Descriptor.ServiceType == typeof(ServiceD));
+    }
+
+    [Theory]
+    [InlineData(0, 4)]
+    [InlineData(1, 4)]
+    public void GetRegistrationChainsByDepth_DepthFiltering(int minDepth, int expectedChains)
+    {
+        var services = CreateTestServices();
+        var tree = new DependencyTree(services, "DependencyInjection.Visualization");
+
+        var result = tree.GetRegistrationChainsByDepth(minDepth);
+
+        result.RootNodes.Should().HaveCount(expectedChains);
+    }
+
+    [Fact]
+    public void GetRegistrationChainsByDepth_UserCodeFiltering()
+    {
+        var services = CreateTestServices();
+        services.AddTransient<string, string>(); // Add a system type
+        var tree = new DependencyTree(services, "DependencyInjection.Visualization");
+
+        var resultWithSystemTypes = tree.GetRegistrationChainsByDepth(1, false);
+        var resultOnlyUserCode = tree.GetRegistrationChainsByDepth(1, true);
+
+        resultWithSystemTypes.RootNodes.Should().HaveCountGreaterThan(resultOnlyUserCode.RootNodes.Count);
+        resultOnlyUserCode.RootNodes.Should().NotContain(n => n.Descriptor.ServiceType == typeof(string));
+    }
+
+    [Fact]
+    public void GetRegistrationChainsByDepth_EmptyAndNullCases()
+    {
+        var emptyServices = new ServiceCollection();
+        var emptyTree = new DependencyTree(emptyServices, "DependencyInjection.Visualization");
+
+        var resultEmpty = emptyTree.GetRegistrationChainsByDepth(1);
+        resultEmpty.RootNodes.Should().BeEmpty();
+
+        var services = CreateTestServices();
+        var tree = new DependencyTree(services, null);
+
+        var resultNullPrefix = tree.GetRegistrationChainsByDepth(1);
+        resultNullPrefix.RootNodes.Should().NotBeEmpty();
+    }
+}
