ad96306b0270429557c4f2d716f2c3027f05bd87
jackkendall <jkendall3096@gmail.com>
2024-07-28 08:24:20 +0100

    Pull out calculating dependency usage to own class

--- DependencyInjection.Visualization/DependencyTree.cs
+++ DependencyInjection.Visualization/DependencyTree.cs
@@ -24,56 +24,6 @@ public class DependencyTree
         _rootNodes = _treeBuilder.BuildTree(services);
     }
     
-    private Dictionary<Type, int>? _dependencyUsage;
-    
-    public Dictionary<Type, int> CalculateDependencyUsage(IEnumerable<ServiceNode> rootNodes)
-    {
-        if (_dependencyUsage is not null)
-        {
-            return _dependencyUsage;
-        }
-        
-        var usageCount = new Dictionary<Type, int>();
-
-        void TraverseNode(ServiceNode node)
-        {
-            foreach (var dependency in node.Dependencies)
-            {
-                var serviceType = dependency.Descriptor.ServiceType;
-                usageCount[serviceType] = usageCount.TryGetValue(serviceType, out var count) ? count + 1 : 1;
-                TraverseNode(dependency);
-            }
-        }
-
-        foreach (var rootNode in rootNodes)
-        {
-            TraverseNode(rootNode);
-        }
-
-        _dependencyUsage = usageCount;
-        
-        return usageCount;
-    }
-    
-    public IEnumerable<(Type ServiceType, int UsageCount)> GetMostUsedServices(IEnumerable<ServiceNode> rootNodes, int count)
-    {
-        var usageCount = CalculateDependencyUsage(rootNodes);
-        
-        return usageCount
-            .OrderByDescending(kvp => kvp.Value)
-            .Take(count)
-            .Select(kvp => (kvp.Key, kvp.Value));
-    }
-
-    public IEnumerable<Type> GetUnusedServices(IEnumerable<ServiceNode> rootNodes)
-    {
-        var usageCount = CalculateDependencyUsage(rootNodes);
-        var allServices = rootNodes.Select(n => n.Descriptor.ServiceType);
-        var unusedServices = allServices.Except(usageCount.Keys);
-
-        return unusedServices.Where(TypeRelevance.IsRelevantType);
-    }
-    
     /// <summary>
     /// Retrieves dependency chains that meet or exceed a specified depth.
     /// </summary>
---
+++ DependencyInjection.Visualization/DependencyUsageCalculator.cs
@@ -0,0 +1,54 @@
+namespace DependencyInjection.Visualization;
+
+public class DependencyUsageCalculator
+{
+    private Dictionary<Type, int>? _dependencyUsage;
+
+    public IEnumerable<(Type ServiceType, int UsageCount)> GetMostUsedServices(IEnumerable<ServiceNode> rootNodes, int count)
+    {
+        var usageCount = CalculateDependencyUsage(rootNodes);
+        
+        return usageCount
+            .OrderByDescending(kvp => kvp.Value)
+            .Take(count)
+            .Select(kvp => (kvp.Key, kvp.Value));
+    }
+
+    public IEnumerable<Type> GetUnusedServices(IEnumerable<ServiceNode> rootNodes)
+    {
+        var usageCount = CalculateDependencyUsage(rootNodes);
+        var allServices = rootNodes.Select(n => n.Descriptor.ServiceType);
+        var unusedServices = allServices.Except(usageCount.Keys);
+
+        return unusedServices.Where(TypeRelevance.IsRelevantType);
+    }
+
+    private Dictionary<Type, int> CalculateDependencyUsage(IEnumerable<ServiceNode> rootNodes)
+    {
+        if (_dependencyUsage is not null)
+        {
+            return _dependencyUsage;
+        }
+        
+        var usageCount = new Dictionary<Type, int>();
+
+        void TraverseNode(ServiceNode node)
+        {
+            foreach (var dependency in node.Dependencies)
+            {
+                var serviceType = dependency.Descriptor.ServiceType;
+                usageCount[serviceType] = usageCount.TryGetValue(serviceType, out var count) ? count + 1 : 1;
+                TraverseNode(dependency);
+            }
+        }
+
+        foreach (var rootNode in rootNodes)
+        {
+            TraverseNode(rootNode);
+        }
+
+        _dependencyUsage = usageCount;
+        
+        return usageCount;
+    }
+}
