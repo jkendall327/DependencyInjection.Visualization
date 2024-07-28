ebf4958af0e0105cc893a05fc6acd755b87ad67b
jackkendall <jkendall3096@gmail.com>
2024-07-28 08:42:31 +0100

    Refactor the tree builder

--- DependencyInjection.Visualization/TreeBuilder.cs
+++ DependencyInjection.Visualization/TreeBuilder.cs
@@ -6,7 +6,7 @@ namespace DependencyInjection.Visualization;
 internal class TreeBuilder
 {
     /// <summary>
-    /// Builds a simple tree structure representing the dependencies between services.
+    /// Builds a simple tree representing the dependencies between services.
     /// </summary>
     /// <param name="services">The collection of ServiceDescriptor objects to analyze.</param>
     /// <returns>A list of ServiceNode objects representing the root nodes of the dependency tree: that is, the services directly added to the <see cref="IServiceCollection"/>.</returns>
@@ -41,7 +41,8 @@ internal class TreeBuilder
         List<ServiceDescriptor> serviceDescriptors,
         HashSet<Type> visitedTypes)
     {
-        if (!visitedTypes.Add(type)) return; // Prevents circular dependencies
+        // Prevents circular dependencies
+        if (!visitedTypes.Add(type)) return; 
         
         var constructor = GetConstructor(type, serviceDescriptors);
     
@@ -51,34 +52,29 @@ internal class TreeBuilder
         {
             var dependency = serviceDescriptors.FirstOrDefault(sd => sd.ServiceType == parameter.ParameterType);
 
-            if (dependency != null)
-            {
-                var childNode = new ServiceNode(dependency);
-                parentNode.Dependencies.Add(childNode);
+            if (dependency is null) continue;
+            
+            var childNode = new ServiceNode(dependency);
+            parentNode.Dependencies.Add(childNode);
 
-                if (dependency.ImplementationType != null)
-                {
-                    BuildDependencies(childNode, dependency.ImplementationType, serviceDescriptors, [..visitedTypes]);
-                }
+            if (dependency.ImplementationType != null)
+            {
+                BuildDependencies(childNode, dependency.ImplementationType, serviceDescriptors, [..visitedTypes]);
             }
         }
     }
 
+    /// <summary>
+    /// We try here to filter out constructors that wouldn't be considered by Microsoft.Extensions.DependencyInjection.
+    /// Namely we care about public constructors with parameters whose types are registered in the container.
+    /// </summary>
     private ConstructorInfo? GetConstructor(Type type, List<ServiceDescriptor> serviceDescriptors)
     {
-        var publicConstructors = type.GetConstructors(BindingFlags.Public | BindingFlags.Instance)
+        var publicConstructors = type
+            .GetConstructors(BindingFlags.Public | BindingFlags.Instance)
             .OrderByDescending(c => c.GetParameters().Length);
 
-        foreach (var constructor in publicConstructors)
-        {
-            if (CanResolveConstructor(constructor, serviceDescriptors))
-            {
-                return constructor;
-            }
-        }
-
-        // If no suitable public constructor is found, return null
-        return null;
+        return publicConstructors.FirstOrDefault(constructor => CanResolveConstructor(constructor, serviceDescriptors));
     }
 
     private bool CanResolveConstructor(ConstructorInfo constructor, List<ServiceDescriptor> serviceDescriptors)
