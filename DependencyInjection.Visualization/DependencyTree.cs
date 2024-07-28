b229c5a265e4c9591dba9466768dfb71bde5e9ec
jackkendall <jkendall3096@gmail.com>
2024-07-28 08:28:34 +0100

    Extract constructing display of tree to own class

--- DependencyInjection.Visualization/DependencyTree.cs
+++ DependencyInjection.Visualization/DependencyTree.cs
@@ -10,15 +10,14 @@ namespace DependencyInjection.Visualization;
 public class DependencyTree
 {
     private readonly TreeBuilder _treeBuilder = new();
+    private readonly TreeViewer _treeViewer = new();
+    
     private readonly List<ServiceNode> _rootNodes;
 
     /// <summary>
     /// Initializes a new instance.
     /// </summary>
     /// <param name="services">The <see cref="IServiceCollection"/> to analyze.</param>
-    /// <param name="assemblyNamePrefix">Optional prefix to filter relevant types. If null, it's determined automatically by calling <see cref="Assembly.GetExecutingAssembly"/>.</param>
-    /// <remarks>You </remarks>
-    /// <example></example>
     public DependencyTree(IServiceCollection services)
     {
         _rootNodes = _treeBuilder.BuildTree(services);
@@ -42,7 +41,7 @@ public class DependencyTree
             }
         }
 
-        var stringRepresentation = GenerateTreeView(deepChains);
+        var stringRepresentation = _treeViewer.GenerateTreeView(deepChains);
         
         return new(deepChains, stringRepresentation);
     }
@@ -100,85 +99,6 @@ public class DependencyTree
     /// </example>
     public string GenerateTreeView(bool onlyUserCode = false)
     {
-        var nodes = _rootNodes;
-        
-        if (onlyUserCode)
-        {
-            nodes = nodes.Where(n => TypeRelevance.IsRelevantType(n.Descriptor.ServiceType)).ToList();
-        }
-        
-        return GenerateTreeView(nodes);
-    }
-
-    private string GenerateTreeView(IEnumerable<ServiceNode> nodes)
-    {
-        var sb = new StringBuilder();
-        var groupedNodes = nodes
-            .GroupBy(n => n.Descriptor.ServiceType.Namespace)
-            .OrderBy(g => g.Key);
-
-        foreach (var group in groupedNodes)
-        {
-            sb.AppendLine($"Namespace: {group.Key}");
-            sb.AppendLine(new('-', 50));
-
-            foreach (var node in group)
-            {
-                AppendNodeAndDependencies(sb, node, 0);
-            }
-
-            sb.AppendLine();
-        }
-
-        return sb.ToString();
-    }
-
-    private void AppendNodeAndDependencies(StringBuilder sb, ServiceNode node, int depth)
-    {
-        sb.AppendLine($"{new string(' ', depth * 2)}{GetServiceDescription(node.Descriptor)}");
-        
-        foreach (var child in node.Dependencies)
-        {
-            AppendNodeAndDependencies(sb, child, depth + 1);
-        }
-    }
-
-    private string GetServiceDescription(ServiceDescriptor descriptor)
-    {
-        var lifetime = descriptor.Lifetime.ToString();
-        var serviceType = descriptor.ServiceType.Name;
-        
-        var implementation = GetImplementationDescription(descriptor);
-        
-        return $"{serviceType} -> {implementation} ({lifetime})";
-    }
-    
-    private string GetImplementationDescription(ServiceDescriptor descriptor)
-    {
-        // For simple cases like .AddSingleton<IFoobar, Foobar>, return Foobar
-        if (descriptor.ImplementationType != null)
-        {
-            return descriptor.ImplementationType.Name;
-        }
-    
-        // When lambdas are used, like .AddSingleton<IFoobar>(x => new Foobar()), get the return type of the lambda
-        if (descriptor.ImplementationFactory != null)
-        {
-            var factoryType = descriptor.ImplementationFactory.GetType();
-            var methodInfo = factoryType.GetMethod("Invoke");
-            
-            if (methodInfo != null)
-            {
-                return methodInfo.ReturnType.Name;
-            }
-        }
-    
-        // When a premade object is used, like .AddSingleton<IFoobar>(foobar), return the type of the instance
-        if (descriptor.ImplementationInstance != null)
-        {
-            return $"Instance of {descriptor.ImplementationInstance.GetType().Name}";
-        }
-
-        return "Unknown";
+        return _treeViewer.GenerateTreeView(_rootNodes, onlyUserCode);
     }
 }
---
+++ DependencyInjection.Visualization/TreeViewer.cs
@@ -0,0 +1,92 @@
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DependencyInjection.Visualization;
+
+public class TreeViewer
+{
+    public string GenerateTreeView(IEnumerable<ServiceNode> rootNodes, bool onlyUserCode = false)
+    {
+        var nodes = rootNodes;
+        
+        if (onlyUserCode)
+        {
+            nodes = nodes.Where(n => TypeRelevance.IsRelevantType(n.Descriptor.ServiceType)).ToList();
+        }
+        
+        return GenerateTreeView(nodes);
+    }
+
+    public string GenerateTreeView(IEnumerable<ServiceNode> nodes)
+    {
+        var sb = new StringBuilder();
+        
+        var groupedNodes = nodes
+            .GroupBy(n => n.Descriptor.ServiceType.Namespace)
+            .OrderBy(g => g.Key);
+
+        foreach (var group in groupedNodes)
+        {
+            sb.AppendLine($"Namespace: {group.Key}");
+            sb.AppendLine(new('-', 50));
+
+            foreach (var node in group)
+            {
+                AppendNodeAndDependencies(sb, node, 0);
+            }
+
+            sb.AppendLine();
+        }
+
+        return sb.ToString();
+    }
+
+    private void AppendNodeAndDependencies(StringBuilder sb, ServiceNode node, int depth)
+    {
+        sb.AppendLine($"{new string(' ', depth * 2)}{GetServiceDescription(node.Descriptor)}");
+        
+        foreach (var child in node.Dependencies)
+        {
+            AppendNodeAndDependencies(sb, child, depth + 1);
+        }
+    }
+
+    private string GetServiceDescription(ServiceDescriptor descriptor)
+    {
+        var lifetime = descriptor.Lifetime.ToString();
+        var serviceType = descriptor.ServiceType.Name;
+        
+        var implementation = GetImplementationDescription(descriptor);
+        
+        return $"{serviceType} -> {implementation} ({lifetime})";
+    }
+    
+    private string GetImplementationDescription(ServiceDescriptor descriptor)
+    {
+        // For simple cases like .AddSingleton<IFoobar, Foobar>, return Foobar
+        if (descriptor.ImplementationType != null)
+        {
+            return descriptor.ImplementationType.Name;
+        }
+    
+        // When lambdas are used, like .AddSingleton<IFoobar>(x => new Foobar()), get the return type of the lambda
+        if (descriptor.ImplementationFactory != null)
+        {
+            var factoryType = descriptor.ImplementationFactory.GetType();
+            var methodInfo = factoryType.GetMethod("Invoke");
+            
+            if (methodInfo != null)
+            {
+                return methodInfo.ReturnType.Name;
+            }
+        }
+    
+        // When a premade object is used, like .AddSingleton<IFoobar>(foobar), return the type of the instance
+        if (descriptor.ImplementationInstance != null)
+        {
+            return $"Instance of {descriptor.ImplementationInstance.GetType().Name}";
+        }
+
+        return "Unknown";
+    }
+}
